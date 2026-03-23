82116501dae6afb9fc47e2f995c539b87120a2ac
varinaldi <vivaldirinaldi@gmail.com>
2026-03-23 00:04:46 -0600

    clean up older folders

--- frontend/scripts/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Formerly this was used to write $CITY_DIR to city-dir.txt but we have since
-# realized this is unnecessary. Keeping for any future entrypoint needs.
-exec "$@"
